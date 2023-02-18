3a02b3c4bc57a82e4145d1433a03933d757f7051
Bober <thedillerthe@gmail.com>
2023-02-18 10:25:06 +0300

    new

---
+++ s21_decimal.c
@@ -0,0 +1,38 @@
+#include "s21_decimal.h"
+
+int s21_is_less(s21_decimal val1, s21_decimal val2) {
+    int res = 0;
+    s21_zero_exp(&val1);
+    s21_zero_exp(&val2);
+    // printf("%d\n", val2.pat.exp);
+    printf("%d\n", val2.pat.sgn);
+
+    if (val1.pat.sgn && !val2.pat.sgn)
+        res = 1;
+    else if (!val1.pat.sgn && val2.pat.sgn)
+        res = 0;
+    else if (val1.pat.exp < val2.pat.exp)
+        res = 1;
+    else if (val1.pat.exp > val2.pat.exp)
+        res = 0;
+    else if (val1.pat.mnt3 < val2.pat.mnt3)
+        res = 1;
+    else if (val1.pat.mnt3 > val2.pat.mnt3)
+        res = 0;
+    else if (val1.pat.mnt2 < val2.pat.mnt2)
+        res = 1;
+    else if (val1.pat.mnt2 > val2.pat.mnt2)
+        res = 0;
+    else if (val1.pat.mnt1 < val2.pat.mnt1)
+        res = 1;
+    else if (val1.pat.mnt1 > val2.pat.mnt1)
+        res = 0;
+
+    return res;
+}
+
+void s21_zero_exp(s21_decimal *val) {
+    val->pat.null1 = 0;
+    val->pat.null2 = 0;
+    if (val->pat.exp > 28) val->pat.exp = 28;
+}
--- s21_decimal.h
+++ s21_decimal.h
@@ -1,15 +1,33 @@
 #ifndef S21_DECIMAL_H
 #define S21_DECIMAL_H
-
+//---
+#include <stdio.h>
+//---
+#include <stdint.h>
 #include <stdlib.h>
 
-typedef struct s21_decimal {
+// typedef struct s21_decimal {
+//     int bits;
+// } s21_decimal;
+
+typedef struct BitPattern {
+    uint32_t mnt1 : 32;
+    uint32_t mnt2 : 32;
+    uint32_t mnt3 : 32;
+    uint16_t null1 : 16;
+    uint8_t exp : 8;
+    uint16_t null2 : 16;
+    uint32_t sgn : 1;
+} BitPattern;
+
+typedef union s21_decimal {
     int bits[4];
+    BitPattern pat;
 } s21_decimal;
 
-typedef union dec_bits {
-    s21_decimal d;
+void s21_zero_exp(s21_decimal *val);
 
-} dec_bits;
+//Операторы сравнение
+int s21_is_less(s21_decimal val1, s21_decimal val2);
 
 #endif  // S21_DECIMAL_H
